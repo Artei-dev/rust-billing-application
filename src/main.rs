75b4f6d9433d2ec935af45e042e5758a9d679019
Artei <jjoestar2000@gmail.com>
2021-08-11 18:52:46 +0200

    Some more crap

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,18 @@ impl Bill {
         println!("Name: {}", self.name);
         println!("Amount Owed: {}", self.amount_owed);
     }
+
+    fn edit(&mut self, name: Option<String>, amount_owed: Option<i32>) {
+        match name {
+            Some(s) => self.name = s,
+            None => (),
+        }
+
+        match amount_owed {
+            Some(s) => self.amount_owed = s,
+            None => (),
+        }
+    }
 }
 
 fn user_input() -> io::Result<String> {
@@ -117,6 +129,100 @@ fn list_bills(bills: &mut HashMap<i32, Bill>) -> Result<(), String> {
     Ok(())
 }
 
+fn remove_bill(bills: &mut HashMap<i32, Bill>) -> Result<(), Error> {
+    input_print("Input bill id: ");
+    let id: String;
+    match user_input() {
+        Ok(input) => id = input,
+        Err(e) => return Err(e),
+    }
+    let id: i32 = id.trim().parse().unwrap();
+
+    input_print("Are you sure you want to delete this bill(yes/no): ");
+    let confirm: String;
+    match user_input() {
+        Ok(input) => confirm = input,
+        Err(e) => return Err(e),
+    }
+    match confirm.to_lowercase().as_str() {
+        "yes" =>  {bills.remove(&id);},
+        "no" => (),
+        _ => (),
+    }
+    Ok(())
+}
+
+fn edit_bill(bills: &mut HashMap<i32, Bill>) -> Result<(), Error>{
+    let id: String;
+    input_print("Input an id: ");
+    match user_input() {
+        Ok(input) => id = input,
+        Err(e) => return Err(e),
+    }
+    let id: i32 = id.trim().parse().unwrap();
+
+    println!("Edit: ");
+    println!("1. Name");
+    println!("2. Owed Amount");
+    println!("3. Both");
+    input_print(">");
+    let choice: String;
+
+    match user_input() {
+        Ok(input) => choice = input,
+        Err(e) => return Err(e),
+    }
+
+    match choice.as_str() {
+        "1" => {
+            let name: String;
+            input_print("Input name: ");
+            match user_input() {
+                Ok(input) => name = input,
+                Err(e) => return Err(e),
+            }
+            bills.get_mut(&id).unwrap().edit(Some(name), None);
+        }
+
+        "2" => {
+            let amount: String;
+            input_print("Input Owed Amount: ");
+            match user_input() {
+                Ok(input) => amount = input,
+                Err(e) => return Err(e),
+            }
+            let amount: i32 = amount.trim().parse().unwrap();
+
+            bills.get_mut(&id).unwrap().edit(None, Some(amount));
+        }
+
+        "3" => {    
+            let name: String;
+            input_print("Input name: ");
+            match user_input() {
+                Ok(input) => name = input,
+                Err(e) => return Err(e),
+            }
+            let amount: String;
+            input_print("Input Owed Amount: ");
+            match user_input() {
+                Ok(input) => amount = input,
+                Err(e) => return Err(e),
+            }
+            let amount: i32 = amount.trim().parse().unwrap();
+            bills.get_mut(&id).unwrap().edit(Some(name), Some(amount));
+        }
+
+    
+
+        _ => println!("There is no such option"),
+    }
+    
+
+    
+    Ok(())
+}
+
 
 fn main() {
     let mut bills = HashMap::new();
@@ -150,9 +256,16 @@ fn main() {
 
             }
             Some(Remove) => {
-
+                match remove_bill(&mut bills) {
+                   Err(e) => println!("Error: {:?}", e),
+                   _ => (),
+                }
             }
             Some(Edit) => {
+                match edit_bill(&mut bills) {
+                    Err(e) => println!("Error: {:?}", e),
+                    _ => (),
+                }
 
             }
             Some(Exit) => {
@@ -163,3 +276,4 @@ fn main() {
         }
     }
 }
+
